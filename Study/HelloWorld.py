fa4906bb9b82115dcedf0e98ff37e8e59f222a1d
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-05-01 23:22:53 +0900

    Create HelloWorld.py

---
+++ Study/HelloWorld.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+# This is annotation!
+
+print('hello world')
+print('my first phython programming...\n')
+print('test')
+print\
+       ('test backslash')
+print('hello? my name is baedi!' + \
+      ' nice to meet you!')
+
+print     ('test jump')
