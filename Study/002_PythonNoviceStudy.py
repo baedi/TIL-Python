b3eddff786851cc531b5d5cb4a7b4a902b65ff9d
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-05-04 22:16:56 +0900

    Python study (2020-05-04)
     - Learn how to declare libraries and how to use features
     - Learn how to view the list of functions in a library
     - Four arithmetic operations, bit operation output test

---
+++ Study/002_PythonNoviceStudy.py
@@ -0,0 +1,38 @@
+# coding:utf-8
+
+# 라이브러리(Library)
+import math
+
+# math 라이브러리 사용
+print('hello world!')
+print(math.sqrt(16))
+print(math.sin(90))
+print(round(math.pi, 2))
+print()
+
+# 연산 (Operation)
+print(3 + 5)   # 덧셈
+print(10 - 5)  # 뺄셈
+print(20 * 3)  # 곱셈
+print(100 / 4) # 나눗셈
+print(10 % 3)  # 나머지
+print(2 ** 12) # 제곱 (2의 12승)
+print()
+
+# 비트 연산 (Bit Operation)
+print(1 | 0)            # OR
+print(0 | 0 | 0) 
+print(True | False)
+
+print(1 & 1)             # AND
+print(0 & 1)
+
+print(~1)                # NOT
+print(~0)
+
+print(32 > 1)           # 부등호
+print(100 == 100)
+print(53 <= 52)
+
+# 라이브러리 사용 시 Tab를 누르면 해당 라이브러리의 함수 목록 확인 가능.
+# ex) math. 입력하고 Tab 클릭
