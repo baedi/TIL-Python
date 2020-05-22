16ed29c95301b88207e9ccb24166c8ed4699cceb
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-05-22 23:46:13 +0900

    Python study (2020-05-22)
    * Update python script (Study/study200521_FileTest)

--- Study/study200521_FileTest/FileMain.py
+++ Study/study200521_FileTest/FileMain.py
@@ -7,28 +7,27 @@ fileManager = FileManager.FileManager()
 select = None
 
 while True :
-    print("[SELECT] (1 : CREATE | 2 : READ | 3 : APPEND | 4 : WRITE | 0 : EXIT) ")
+    print("[SELECT] (1 : CREATE | 2 : READ | 3 : APPEND | 0 : EXIT) ")
     select = int(input("Input : "))
-    
+
     if select == 1 :
         fileManager.CreateFile(input("File name : "))
         fileManager.CloseFile()
-        
+
     elif select == 2 :
         print("Read file...\n")
         fileManager.ReadFile(input("File name : "))
         fileManager.CloseFile()
-        
+
     elif select == 3 :
-        print('')
-        
-    elif select == 4 :
-        print('')
-        
+        print('Append file...\n')
+        fileManager.AppendFile(input("File name : "))
+        fileManager.CloseFile()
+
     elif select == 0 :
         print("Close file...")
         fileManager.CloseFile()
         break
-        
+
     else :
         print("Invalid access...")
--- Study/study200521_FileTest/FileManager.py
+++ Study/study200521_FileTest/FileManager.py
@@ -2,35 +2,36 @@
 
 ### Class.
 class FileManager:
-    
+
     ## Member Variable.
     __file = None
-    
+
     ## Initialization.
     def __init__(self):
         print("File manager initialization...")
-        
+
     def CreateFile(self, name):
         self.__file = open("%s.txt" % (name), 'w')
         print("Success create file!")
-        
+
     def ReadFile(self, name):
         self.__file = open("%s.txt" % (name), 'r')
-        
+
         while True :
             linestr = self.__file.readline()
             if not linestr: break
             print(linestr)
-            
-            
-        
+
     def AppendFile(self, name):
         self.__file = open("%s.txt" % (name), 'a')
-        
+        input_ = input("Input : ")
+
+        self._file.write(input_)
+
     def CloseFile(self):
-        if self.__file != None : 
+        if self.__file != None :
             self.__file.close();
-    
+
     ## Get _file Reference Method.
     def GetFileReference(self):
         return __file
