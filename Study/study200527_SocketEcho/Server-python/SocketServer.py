db3c47972639a5456cbb6fda712f2b0e11684d9f
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-05-31 22:50:38 +0900

    Python study (2020-05-31)

--- Study/study200527_SocketEcho/Server-python/SocketServer.py
+++ Study/study200527_SocketEcho/Server-python/SocketServer.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 class SockServer:
     
@@ -7,12 +8,17 @@ class SockServer:
     __port = 50000
     __server_sock = None
     __client_sock = None
+    __thread = None
+    __threadOn = False
     
     
     ## Initialization.
     def __init__(self, host):
         ## Set host and port
         self.__host = host
+        self.__thread = threading.Thread(target=self.ReceiveMessage)
+        self.__thread.daemon = True
+        self.__threadOn = True
         pass
         
         
@@ -20,29 +26,57 @@ class SockServer:
     def OpenServer(self):
         self.__server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        print("%s:%d" % (self.__host, self.__port))
+        print("%s:%d\n" % (self.__host, self.__port))
         self.__server_sock.bind((self.__host, self.__port))
         
         # Wait client connect...
-        print("Wait...")
+        print("Wait...\n")
         self.__server_sock.listen()
         self.__client_sock, addr = self.__server_sock.accept()
         
-        print("Client connected! (info : %s)" % (str(addr)))
-
-        # receive message...
-        message = self.__client_sock.recv(4)
-        print("Received!")
-        length = int.from_bytes(message, "big")
+        print("Client connected! (info : %s)\n" % (str(addr)))
+        pass
+    
+    
+    ## Receive Message. 
+    def ReceiveMessage(self):
         
-        message = self.__client_sock.recv(length)
-        print("Client : %s" % (message.decode('utf-8')))
+        while True:
+            # receive message...
+            message = self.__client_sock.recv(4)
+            length = int.from_bytes(message, "big")
         
+            message = self.__client_sock.recv(length)
+            print("Client : %s\n" % (message.decode('utf-8')))
+            self.__client_sock.sendall(message)
+            pass
+    
+        pass
+    
+    
+    ## Send Message
+    def SendMessage(self):
         
+        while True:
+            
+            message = input()
+            self.__client_sock.send(message.encode())
+            
+            if str(message) == " " : break
+            pass
+        pass
+    
+    
+    
+    def GetThread(self): return self.__thread
+    
+    
+    def StopThread(self):
+        self.__threadOn = False
+        self.__thread.join()
         
-        self.__client_sock.sendall(message)
-        
-        
+    
+    def DisconectSever(self):
         # Disconnect.
         self.__client_sock.close()
         self.__server_sock.close()
@@ -56,3 +90,8 @@ host = input("Host : ")
 
 server = SockServer(str(host))
 server.OpenServer()
+
+server.GetThread().start()
+server.SendMessage()
+
+server.DisconectSever()
