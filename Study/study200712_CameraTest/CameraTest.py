fbc94422b5087033a4ccf582e19a37e429224b80
Daeil Bae <47047263+baedi@users.noreply.github.com>
2020-07-13 12:55:52 +0900

    Python study (2020-07-13)
    - How to resize the screen
    - How to add and set screen text
    - Adding recording time using timer

--- Study/study200712_CameraTest/CameraTest.py
+++ Study/study200712_CameraTest/CameraTest.py
@@ -6,11 +6,22 @@ class CameraPreview:
     
     ### Variable
     __camera = None
+    __width = 1280
+    __height = 720
+    __frame = 12
+    
+    __recordTime = -1
+    
     
     ### Initialization
     def __init__(self):
         print("Camera initialization...")
         self.__camera = picamera.PiCamera()
+        self.__camera.resolution = (self.__width, self.__height)
+        self.__camera.framerate = self.__frame
+        self.__camera.annotate_text_size = 24
+        self.__camera.annotate_background = picamera.Color('black')
+        self.CamTimer()
         pass
         
     def StartPreview(self):
@@ -32,6 +43,24 @@ class CameraPreview:
         print("Stop Recording!")
         pass
     
+    def CamTimer(self):
+        timer = threading.Timer(1, self.CamTimer)
+        timer.start()
+        
+        self.__recordTime = self.__recordTime + 1
+        pass
+    
+    def UpdateText(self):
+        second = (int)(self.__recordTime % 60)
+        minute = (int)((self.__recordTime % 3600) / 60)
+        hour = (int)(self.__recordTime / 3600)
+        
+        timeStr = str(hour).zfill(2) + ":" + str(minute).zfill(2) + ":" + str(second).zfill(2)
+        
+        self.__camera.annotate_text = " Date : (comming soon) \n" + " Record time : %s " % (timeStr)
+        pass
+    
+    
     pass
 
 
@@ -44,13 +73,14 @@ cameraManager.StartRecording()
 
 try:
     while isLoop:
+        cameraManager.UpdateText()
         pass
     pass
 
 except KeyboardInterrupt:
     isLoop = False
+    cameraManager.StopRecording()    
     cameraManager.StopPreview()
-    cameraManager.StopRecording()
     pass
 
 print("Program exit")
---
+++ Study/study200712_CameraTest/Reference.md
@@ -0,0 +1,3 @@
+- [PiCamera Library (파이 카메라 라이브러리)](https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/7)
+- [Timer (타이머)](http://blog.naver.com/PostView.nhn?blogId=nasu0210&logNo=220737052435)
+- [Zerofill (공백 채우기)](https://brownbears.tistory.com/483)
